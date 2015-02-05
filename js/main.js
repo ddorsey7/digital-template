9edae10a93f47b63103d4ed395f1d7ab0acbf7f7
ddorsey7 <ddorsey7@users.noreply.github.com>
2015-02-05 03:31:58 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -13,7 +13,7 @@ window.onload = function() {
     
     "use strict";
     
-    var game = new Phaser.Game(800, 600, Phaser.CANVAS, 'phaser-example', { preload: preload, create: create, update: update, render: render });
+    var game = new Phaser.Game(800, 600, Phaser.CANVAS, 'game', { preload: preload, create: create, update: update, render: render });
 
 function preload() {
 
