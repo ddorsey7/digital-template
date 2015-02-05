8ff762d39c6298dae6ff8bf012ad1e19dfa424a9
ddorsey7 <ddorsey7@users.noreply.github.com>
2015-02-05 01:13:34 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -17,8 +17,8 @@ window.onload = function() {
 
     function preload() {
 
-        game.load.image('logo', 'assets/phaser.png');
-        game.load.image('bullet', 'assets/rocket.png');
+        game.load.image('bullet', 'assets/bullet.png');
+        game.load.image('rock', 'assets/rocket.png');
     
     }
 
@@ -38,7 +38,7 @@ window.onload = function() {
         bullets.enableBody = true;
         bullets.physicsBodyType = Phaser.Physics.ARCADE;
 
-        bullets.createMultiple(50, 'logo');
+        bullets.createMultiple(50, 'bullet');
     bullets.setAll('checkWorldBounds', true);
     bullets.setAll('outOfBoundsKill', true);
     
